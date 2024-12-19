c4e9413674b956767f95fdf82ba7ca83b82579c9
Krrish Sehgal <133865424+krrish-sehgal@users.noreply.github.com>
2024-12-20 00:37:18 +0530

    hel

--- check.py
+++ check.py
@@ -1,12 +1,10 @@
-
 def process_data(data: list, limit: int = 100):
-processed = [d for d in data if d < limit]
-return processed
+    processed = [d for d in data if d < limit]
+    return processed
 
 
 from django.db import models
 
 class User(models.Model):
-username = models.CharField(max_length=150)
-email = models.EmailField(unique=True)
-
+    username = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
